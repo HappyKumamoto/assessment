be77b6103242afae2112fdbb425f1689fc07253a
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2020-08-16 13:04:22 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -60,38 +60,39 @@
   };
 
   const answers = [
-    '{userName}んよかところは声ばい。{userName}ん特徴的な声は皆ば惹きつけ、心に残る。  <br > The nice point of {userName} is the voice. The characteristic voice of {userName} is attractive and memorable. <br>{userName}のよきところは声なり。{userName}の特徴やうなる声はみな惹きつけ、心に残る。', 
+    '{userName}んよかところは声ばい。{userName}ん特徴的な声は皆ば惹きつけ、心に残る。  <br > One of the nice point of {userName} is the voice. One of the characteristic voice of {userName} is attractive and memorable. <br>{userName}のよきところは声なり。{userName}の特徴やうなる声はみな惹きつけ、心に残る。', 
     
-    '{userName}んよかところはまなざしばい。{userName}に見つめられた人は、気になって仕方がなかやろう。 <br >The good point of {userName} is the look. Those who are stared at by {userName} can not help but wonder. ',
+    '{userName}んよかところはまなざしばい。{userName}に見つめられた人は、気になって仕方がなかやろう。 <br >One of the good point of {userName} is the look. Those who are stared at by {userName} can not help but wonder. ',
     
-    '{userName}んよかところは情熱ばい。{userName}ん情熱に周りん人は感化さるる。 <br >The good point of {userName} is passion. The passion of {userName} inspires others. ',
+    '{userName}んよかところは情熱ばい。{userName}ん情熱に周りん人は感化さるる。 <br >One of the good point of {userName} is passion. One of the passion of {userName} inspires others. ',
     
-    '{userName}んよかところは厳しさばい。{userName}ん厳しさがもんごとばいつも成功に導く。 <br >The nice thing about {userName} is the rigor. The severity of {userName} always leads to success. ',
+    '{userName}んよかところは厳しさばい。{userName}ん厳しさがもんごとばいつも成功に導く。 <br >One of the nice thing about {userName} is the rigor. One of the severity of {userName} always leads to success. ',
     
-    '{userName}んよかところは知識ばい。博識な{userName}ば多うん人が頼りにしとる。 <br >The good thing about {userName} is knowledge. Many people rely on your wisdom {userName}.' ,
+    '{userName}んよかところは知識ばい。博識な{userName}ば多うん人が頼りにしとる。 <br >One of the good thing about {userName} is knowledge. Many people rely on your wisdom {userName}.' ,
     
-    '{userName}んよかところはユニークさばい。{userName}だけんそん特徴が皆ば楽しゅうさする。 <br >The nice thing about {userName} is uniqueness. Your unique feature of {userName} makes everyone happy. ',
+    '{userName}んよかところはユニークさばい。{userName}だけんそん特徴が皆ば楽しゅうさする。 <br >One of the nice thing about {userName} is uniqueness. Your unique feature of {userName} makes everyone happy. ',
     
-    '{userName}んよかところは用心深さばい。{userName}ん洞察に、多うん人が助けらるる。 <br > The nice thing about {userName} is caution. Many people can be helped by the insight of {userName}. ',
+    '{userName}んよかところは用心深さばい。{userName}ん洞察に、多うん人が助けらるる。 <br > One of the nice thing about {userName} is caution. Many people can be helped by the insight of {userName}. ',
     
-    '{userName}んよかところは見た目ばい。内側から溢れ出る{userName}ん良さに皆が気ば惹かるる。 <br >The nice thing about {userName} is how you looks. Everyone is drawn to the goodness of the {userName} that overflows from the inside. ',
+    '{userName}んよかところは見た目ばい。内側から溢れ出る{userName}ん良さに皆が気ば惹かるる。 <br >One of the nice thing about {userName} is how you looks. Everyone is drawn to the goodness of the {userName} that overflows from the inside. ',
     
-    '{userName}んよかところは決断力ばい。{userName}がする決断にいつも助けらるる人がおる。 <br >The good thing about {userName} is that your deterministic. {userName} helps people by making decisions. <br>{userName}のよきところは決断力なり。{userName}のする決断に日ごろ助けらるる人あり。',
+    '{userName}んよかところは決断力ばい。{userName}がする決断にいつも助けらるる人がおる。 <br >One of the good thing about {userName} is that your deterministic. {userName} helps people by making decisions. <br>{userName}のよきところは決断力なり。{userName}のする決断に日ごろ助けらるる人あり。',
     
-    '{userName}んよかところは思いやりばい。{userName}に気ばかけてもろうた多うん人が感謝しとる。 <br >The nice thing about {userName} is compassion. Many people are grateful for you. ',
+    '{userName}んよかところは思いやりばい。{userName}に気ばかけてもろうた多うん人が感謝しとる。 <br >One of the nice thing about {userName} is compassion. Many people are grateful for you. ',
     
-    '{userName}んよかところは感受性ばい。{userName}が感じたことに皆が共感し、わかりあいきる。 <br >The nice thing about {userName} is sensitivity. Everyone can empathize and understand what {userName} feels. ',
+    '{userName}んよかところは感受性ばい。{userName}が感じたことに皆が共感し、わかりあいきる。 <br >One of the nice thing about {userName} is sensitivity. Everyone can empathize and understand what {userName} feels. ',
     
-    '{userName}んよかところは節度ばい。強引すぎん{userName}ん考えに皆が感謝しとる。 <br >The good thing about {userName} is that it has a modest attitude. Everyone is grateful to you for not pushing your thoughts. ',
+    '{userName}んよかところは節度ばい。強引すぎん{userName}ん考えに皆が感謝しとる。 <br >One of the good thing about {userName} is that it has a modest attitude. Everyone is grateful to you for not pushing your thoughts. ',
     
-    '{userName}んよかところは好奇心ばい。新しかことに向かっていく{userName}ん心構えが多うん人に魅力的に映る。 <br >The good thing about {userName} is curiosity. Your attitude towards new things will appeal to many people. ',
+    '{userName}んよかところは好奇心ばい。新しかことに向かっていく{userName}ん心構えが多うん人に魅力的に映る。 <br >One of the good thing about {userName} is curiosity. Your attitude towards new things will appeal to many people. ',
     
-    '{userName}んよかところは気配りばい。{userName}ん配慮が多うん人ば救うとる <br >The nice thing about {userName} is that you can be attentive. You are saving a lot of people. ',
+    '{userName}んよかところは気配りばい。{userName}ん配慮が多うん人ば救うとる <br >One of the nice thing about {userName} is that you can be attentive. You are saving a lot of people. ',
     
-    '{userName}んよかところはそん全てばい。ありんままん{userName}自身がよかところなんや。 <br >The good thing about {userName} is that all. You are what you are. ',
+    '{userName}んよかところはそん全てばい。ありんままん{userName}自身がよかところなんや。 <br >One of the good thing about {userName} is that all. You are what you are. ',
     
-    '{userName}んよかところは自制心ばい。やばかて思うたときにしっかと衝動ば抑えらるる{userName}が皆から評価されとる。 <br > The nice thing about {userName} is self-control. You are praised for ability to control your urges in times of danger. ' ,
+    '{userName}んよかところは自制心ばい。やばかて思うたときにしっかと衝動ば抑えらるる{userName}が皆から評価されとる。 <br > One of the nice thing about {userName} is self-control. You are praised for ability to control your urges in times of danger. ' ,
   ];
+
 
   /**
    * 名前の文字列を渡すと診断結果を返す関数
